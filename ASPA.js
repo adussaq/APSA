a0fdd01be12258fa8a64b67a7952137710c73075
Alex Dussaq <a.m.dussaq@gmail.com>
2013-01-10 16:01:31 -0600

    More with tabs

--- ASPA.js
+++ ASPA.js
@@ -9,21 +9,13 @@
 
 
 	//Make tabs
-	main.html('<ul class="nav nav-tabs"><li><a href="#home" data-toggle="tab">Home</a></li><li><a href="#profile" data-toggle="tab">Profile</a></li><li><a href="#messages" data-toggle="tab">Messages</a></li><li><a href="#settings" data-toggle="tab">Settings</a></li></ul>');
+	main.html('<ul class="nav nav-tabs"><li><a href="#constitution" data-toggle="tab">Constitution</a>This will be the constitution...</li><li><a href="#profiles" data-toggle="tab">Profiles</a>These will be student profiles</li></ul>');
 
-	$('#home').click(function (e) {
+	$('#constitution').click(function (e) {
 		e.preventDefault();
 		$(this).tab('show');
 	});
 	$('#profiles').click(function (e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
-	$('#messages').click(function (e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
-	$('#home').click(function (e) {
 		e.preventDefault();
 		$(this).tab('show');
 	});
