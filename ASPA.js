8fa7e83257cf6ccd8c27e909c10d1f35a439378c
Alex Dussaq <a.m.dussaq@gmail.com>
2013-01-10 13:42:00 -0600

    Basic page stuff

--- ASPA.js
+++ ASPA.js
@@ -1,1 +1,11 @@
-console.log('Hello from github');
+/*global console, $ */
+(function () {
+	'use strict';
+	var main;
+
+	console.log('Hello from github');
+
+	main = $('#ASPAmain');
+	main.text("Travis I have access, what do you want me to add?");
+
+}());
