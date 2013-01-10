642b113862f4aa474fc7d3f313cf8b757d2db7a4
Alex Dussaq <a.m.dussaq@gmail.com>
2013-01-10 16:04:26 -0600

    Remove tabs to try again

--- ASPA.js
+++ ASPA.js
@@ -5,20 +5,13 @@
 	main = $('#ASPAmain');
 
 	//load scripts
-	$('<script />', {src: 'https://raw.github.com/adussaq/ASPA2/master/bootstrap.min.js'}).appendTo(main);
+	//$('<script />', {src: 'https://raw.github.com/adussaq/ASPA2/master/bootstrap.min.js'}).appendTo(main);
 
 
 	//Make tabs
-	main.html('<ul class="nav nav-tabs"><li><a href="#constitution" data-toggle="tab">Constitution</a>This will be the constitution...</li><li><a href="#profiles" data-toggle="tab">Profiles</a>These will be student profiles</li></ul>');
-
-	$('#constitution').click(function (e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
-	$('#profiles').click(function (e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
+	
+	
+	
 
 	console.log('Hello from github');
 
