e1d4aea606a0cb814ce0e771cb7d1943bc8a0493
Alex Dussaq <a.m.dussaq@gmail.com>
2013-01-11 08:45:38 -0600

    Add temp tabs...

--- ASPA.js
+++ ASPA.js
@@ -10,7 +10,22 @@
 
 
 	//Make tabs
-	
+	main = $('#ASPAmain');
+$('<div />', {html: '<a href="#" id="tab1">Tab1</a><div class= "hide">This is text</div>'}).appendTo(main);
+$('<div />', {html: '<a href="#" id="tab2">Tab2</a><div class= "hide">This is also text</div>'}).appendTo(main);
+$('.hide').hide();
+tabClick = function (evt) {
+var text;
+    evt.preventDefault();
+    text = $($(this).parent().children('div')[0]);
+    if( text.is(":visible") ) {
+        $('.hide').hide('slow');
+    } else {
+        $('.hide').hide({duration:'slow', complete:function () { text.show();}});
+    }
+};
+$('#tab1').click(tabClick);
+$('#tab2').click(tabClick);	
 	
 	
 
