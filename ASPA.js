3ce21fee76eef5c3f35030734cc6b16f32305eea
Alex Dussaq <a.m.dussaq@gmail.com>
2013-01-14 13:54:07 -0600

    Hopefully make them into slides

--- ASPA.js
+++ ASPA.js
@@ -65,7 +65,14 @@
 
 	//Actually create the this is me section
 	(function () {
-		var i, j, scale;
+		var i, j, scale, thisIsMe, slideClickLast, slideClick;
+
+		slideClick = function () {
+			$(this).hide('slow');
+		};
+		slideClickLast = function () {
+			$('#slideIMG').show('slow');
+		};
 
 		scale = 2;
 		//Sort this is me array
@@ -103,9 +110,7 @@
 
 			//Add the images for this person, hiding all but the first one...
 			for (j = 0; j < thisIsMeObj[i].images.length; j += 1) {
-				$('<img />', {style: "display:block;position:relative;width:" + 702 / scale + "px;", src: 'https://raw.github.com/adussaq/ASPA2/master/thisIsMeImages/' + thisIsMeObj[i].images[j]}).click(function (evt) {
-					$(this).hide('slow');
-				}).appendTo('#slideContent' + i);
+				$('<img />', {'class': 'slideIMG', style: "display:block;position:relative;width:" + 702 / scale + "px;", src: 'https://raw.github.com/adussaq/ASPA2/master/thisIsMeImages/' + thisIsMeObj[i].images[j]}).click(j === thisIsMeObj[i].images.length - 1 ? slideClickLast : slideClick).appendTo('#slideContent' + i);
 			}
 
 		}
