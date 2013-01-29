867dca7706cafe8b21bd295158525dec456bc1f2
Alex Dussaq <a.m.dussaq@gmail.com>
2013-01-29 12:20:28 -0600

    Change spacing a bit

--- ASPA.js
+++ ASPA.js
@@ -20,32 +20,32 @@
 	//Make tabs/set up page
 	main = $('#ASPAmain');
 	//About us
-	$('<div />', {html: '<a href="#" id="tab1">About Us</a><div class= "hide">' +
+	$('<div />', {style: "margin-left:10px", html: '<a href="#" id="tab1">About Us</a><div class= "hide">' +
 						'&nbsp;&nbsp;The MSTP is excited to announce the formation of an American Physician Scientists Association (APSA) chapter at UAB! The goals of our APSA chapter are multifaceted, as we aim to address the unique challenges that face physician-scientists during their training and career. Among other things, we will provide members with opportunities for informal networking with UAB faculty, organize career development and volunteering events, and provide a formal organization through which UAB’s future physician-scientists can have an active voice both nationally and among UAB’s scientific community.' +
 						'<br />&nbsp;&nbsp;APSA is a national organization that was formed by MD/PhD students to address the needs and unique challenges faced by physician-scientists during their training. The national APSA chapter is run entirely by MD/PhD students from institutions across the country.  The organization advocates for policy changes essential to the continued success of physician-scientists, and the NCI’s recent establishment of an F30 for MD/PhD students is an example of APSA’s influence and frequent success. The association also sponsors events such as conferences and webinars that are developed specifically for aspiring-physician scientists. For more information, please visit the national chapter’s website (<a href=http://www.physicianscientists.org>http://www.physicianscientists.org</a>), which contains useful resources, including forums to network with established physician-scientists from around the country and to get specialty-specific mentorship.' +
 						'<br />&nbsp;&nbsp;The UAB MSTP was interested in forming an APSA chapter because the organization has the potential to strengthen our MSTP, to establish a forum for networking with research-oriented SOM students, and to provide additional resources for career development. Excitingly, our chapter already has over 40 members! Our chapter meets monthly, and meetings consist of research presentations and the planning of future APSA-sponsored events tailored to the needs of our members. Most importantly, our organization is meant to be a resource for our members, so please let us know if you have any ideas for APSA-sponsored events in the future! We believe that our UAB APSA chapter will be an extremely valuable organization for our members and our MSTP, so please consider joining!' +
 						'<br /><br /><a href="https://github.com/adussaq/ASPA2/raw/master/APSAConstitution_2013_01_10.pdf">Download Constitution</a><br /><br /></div>'}).appendTo(main);
 	//Officers
-	$('<div />', {html: '<a href="#" id="tab2">Officers</a><div class= "hide">' +
-						'&nbsp;&nbsp;<b>President:</b> Travis Hull<br />' +
-						'&nbsp;&nbsp;<b>Vice President:</b> Jennifer Stanley<br />' +
-						'&nbsp;&nbsp;<b>Secretary:</b> Stephanie Robert<br />' +
-						'&nbsp;&nbsp;<b>Treasurer:</b> Alice Weaver<br />' +
-						'&nbsp;&nbsp;<b>Webmaster:</b> Alex Dussaq<br />' +
-						'&nbsp;&nbsp;<b>SOM Representative:</b> Meredith Hubbard<br />' +
-						'&nbsp;&nbsp;<b>Faculty Advisor:</b> Dr. Robin Lorenz<br /><br /></div>'}).appendTo(main);
+	$('<div />', {style: "margin-left:10px", html: '<a href="#" id="tab2">Officers</a><div class= "hide">' +
+						'<b>President:</b> Travis Hull<br />' +
+						'<b>Vice President:</b> Jennifer Stanley<br />' +
+						'<b>Secretary:</b> Stephanie Robert<br />' +
+						'<b>Treasurer:</b> Alice Weaver<br />' +
+						'<b>Webmaster:</b> Alex Dussaq<br />' +
+						'<b>SOM Representative:</b> Meredith Hubbard<br />' +
+						'<b>Faculty Advisor:</b> Dr. Robin Lorenz<br /><br /></div>'}).appendTo(main);
 
 	//Create this is membership section
-	$('<div />', {html: '<a href="#" id="tab3">Members</a><div class= "hide">' +
+	$('<div />', {style: "margin-left:10px", html: '<a href="#" id="tab3">Members</a><div class= "hide">' +
 		'&nbsp;&nbsp;This is not an exhaustive list, instead a list of members who wanted us to know "This is me."<br /><br /><div id=thisIsMe></div><div><br /></div>'}).appendTo(main);
 	//Important Links
-	$('<div />', {html: '<a href="#" id="tab4">Important Links</a><div class= "hide">&nbsp;&nbsp;<a href="http://www.physicianscientists.org">ASPA National Chapter</a><br />' +
+	$('<div />', {style: "margin-left:10px", html: '<a href="#" id="tab4">Important Links</a><div class= "hide"><a href="http://www.physicianscientists.org">ASPA National Chapter</a><br />' +
 						'Image courtesy of <a href="http://genomicscience.energy.gov">U.S. Department of Energy Genomic Science program</a><br /><br /></div>'}).appendTo(main);
 	//Important Dates
-	$('<div />', {html: '<a href="#" id="tab5">Important Dates</a><div class= "hide" style="margin-left:10px"><b>January 22nd at 3pm</b> - Radiation Oncology break-out session with Drs. Eddy Yang, MD PhD and Alex Whitley, MD PhD. Email Contact: <a href="mailto:jenn87@uab.edu">Jennifer Stanley</a> for more information. (Limited to 10 people in attendence).<br />' +
+	$('<div />', {style: "margin-left:10px", html: '<a href="#" id="tab5">Important Dates</a><div class= "hide" style="margin-left:10px"><b>January 22nd at 3pm</b> - Radiation Oncology break-out session with Drs. Eddy Yang, MD PhD and Alex Whitley, MD PhD. Email Contact: <a href="mailto:jenn87@uab.edu">Jennifer Stanley</a> for more information. (Limited to 10 people in attendence).<br />' +
 						'<b>Febuary 13th at 5pm</b> - Monthly Meeting, Shelby Biomedical Building, room 105.<br /><br /></div>'}).appendTo(main);
 	//Contact Us
-	$('<div />', {html: '<a href="#" id="tab6">Contact Us</a><div class= "hide">&nbsp;&nbsp;In development<br /><br /></div>'}).appendTo(main);
+	$('<div />', {style: "margin-left:10px", html: '<a href="#" id="tab6">Contact Us</a><div class= "hide">&nbsp;&nbsp;In development<br /><br /></div>'}).appendTo(main);
 
 
 	//Add some javascript properties to above tabs
