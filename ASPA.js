ef6931dfe04a4490c941a8a35665edc37951189b
Alex Dussaq <a.m.dussaq@gmail.com>
2013-05-21 10:28:15 -0500

    Change image header

--- ASPA.js
+++ ASPA.js
@@ -143,7 +143,7 @@ var getImportantDates, importantDates;
 		slideClick = function (evt) {
 			evt.preventDefault();
 			$(this).hide('slow');
-			$("#slide" + ($(this).data('ind')+1)).show('slow');
+			$("#slide" + ($(this).data('ind') + 1)).show('slow');
 		};
 		slideClickLast = function (evt) {
 			evt.preventDefault();
@@ -152,7 +152,7 @@ var getImportantDates, importantDates;
 		};
 
 		//make the html holder
-		$('<div />', {html: '<b>Images</b><div style="border:2px solid black;' + "width:" + 702 / scale + 'px;overflow: hidden;margin-left: auto;margin-right: auto;" id="eventPicHolder"></div>'}).appendTo(eventDiv);
+		$('<div />', {html: '<div style="text-align: center;"><b>Images</b></ br>Click for next slide. </div><div style="border:2px solid black;' + "width:" + 702 / scale + 'px;overflow: hidden;margin-left: auto;margin-right: auto;" id="eventPicHolder"></div>'}).appendTo(eventDiv);
 
 
 		//Add the images for this person, hiding all but the first one...
