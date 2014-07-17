35e9b004f8ca36e3b53721f314f5d0f179745e48
Alex Dussaq <a.m.dussaq@gmail.com>
2014-07-17 11:39:15 -0500

    add end date for longer events

--- ASPA.js
+++ ASPA.js
@@ -1,6 +1,6 @@
 /*global console, $, jQuery */
 var updateChanges;
-console.log("v2.0.10");
+console.log("v2.0.11");
 //Tracking
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -172,7 +172,7 @@ var getImportantDates, importantDates;
             table = $('<table>', {style:"width:100%"}).appendTo(div);
             for (i = 0; i < rost.length; i += 1) {
                 if(! (i%2)) {
-                    trow = $('<tr>', {style:"width:100%"}).appendTo(table);
+                    trow = $('<tr>', {style:"padding:5px;width:100%;"}).appendTo(table);
                 }
                 indText = $('<span>', {html: "<b>"+ rost[i].name + "</b> " +rost[i].program}); 
                 if (rost[i].hasOwnProperty("office")) {
@@ -181,7 +181,7 @@ var getImportantDates, importantDates;
                 if (rost[i].hasOwnProperty('thisIsMe')) {
                     indText.append(getThisIsMeImages(rost[i].name, rost[i].thisIsMe));
                 }
-                $('<td>', {style:"width:50%", html: indText}).appendTo(trow);
+                $('<td>', {style:"width:50%;padding:5px;", html: indText}).appendTo(trow);
             }
             //Hide everything
             $('.slide').hide();
@@ -206,7 +206,11 @@ var getImportantDates, importantDates;
 
             for (i = 0; i < dates.length; i += 1) {
                 $('<tr>').append(
-                    $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString()})
+                    new Date(dates[i].endTime) - new Date(dates[i].date) > 60*60*1000 ?
+                      $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString() + " - " + (new Date(dates[i].endTime)).toLocaleString() })
+                    :
+                      $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString()})
+                    
                 ).append(
                     $('<td>', {style: "padding:5px;", html: "<b>" + dates[i].name + '</b><br />' + dates[i].location + (dates[i].description ? "<br />" + dates[i].description : "") })
                 ).appendTo(div);
