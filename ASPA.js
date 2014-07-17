63c7d5e93935c657bad710eb5f62a7e6c9a9cda2
Alex Dussaq <a.m.dussaq@gmail.com>
2014-07-17 12:27:38 -0500

    crockford proof

--- ASPA.js
+++ ASPA.js
@@ -1,7 +1,8 @@
 /*global console, $, jQuery */
 var updateChanges;
-console.log("v2.0.12");
+console.log("v2.0.13");
 //Tracking
+///*
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,7 +10,7 @@ console.log("v2.0.12");
 
   ga('create', 'UA-48393146-1', 'uab-apsa.googlecode.com');
   ga('send', 'pageview');
-
+//*/
 var getImportantDates, importantDates;
 (function () {
     'use strict';
@@ -76,7 +77,7 @@ var getImportantDates, importantDates;
     //About us
     $('<div />', {html: '<a href="#" id="tab1">About Us</a><div class= "hide" style="margin-left:10px">' +
                         '&nbsp;&nbsp;At the national level, APSA works to furthering the training and success of physician scientists by providing resources and opportunities for networking, mentorship, career development, and policy change. For more information, please visit the national organization’s website (http://www.physicianscientists.org) and consider becoming an APSA member.' +
-                        '<br />&nbsp;&nbsp;The UAB APSA local chapter was established in Spring 2013 to promote networking within the physician scientist community at UAB, to organize career development events for physician scientists in training, to expand the role of physician scientists into the Birmingham community through outreach events, and to provide a formal platform through which UAB’s future physician-scientists can have an active voice both locally and nationally.  Since then, we have expanded our roster to include over 60 members from the MD, MD/PhD, and MD/MPH programs.  We have sponsored over 50 events ranging from presentations to local high school students on the real-world applications of basic science principles to a roundtable discussion with the CEO of the American Medical Association to workshops on successful grant writing. Each event we sponsor is proposed, planned, and organized by committees composed of APSA members; last year we had over 30 members who served in an active role on these committees.' + 
+                        '<br />&nbsp;&nbsp;The UAB APSA local chapter was established in Spring 2013 to promote networking within the physician scientist community at UAB, to organize career development events for physician scientists in training, to expand the role of physician scientists into the Birmingham community through outreach events, and to provide a formal platform through which UAB’s future physician-scientists can have an active voice both locally and nationally.  Since then, we have expanded our roster to include over 60 members from the MD, MD/PhD, and MD/MPH programs.  We have sponsored over 50 events ranging from presentations to local high school students on the real-world applications of basic science principles to a roundtable discussion with the CEO of the American Medical Association to workshops on successful grant writing. Each event we sponsor is proposed, planned, and organized by committees composed of APSA members; last year we had over 30 members who served in an active role on these committees.' +
                         '<br />&nbsp;&nbsp;The UAB APSA chapter has been recognized by locally and nationally for our dedication to physician scientist training.  We have received multiple nominations for the outstanding UAB student group of the year and have a strong history of support from both the UAB School of Medicine and the UAB Medical Scientist Training Program.  We have received several grants from the national APSA office including an institutional travel award to the 2014 APSA Annual Meeting, which was attended by 9 of our members.  We were also recognized at the 2014 Annual Meeting as a local chapter of excellence and had the opportunity to speak at the APSA business meeting on our success as a student group.  In addition, we currently have 7 local members serving on national APSA planning committees, including 3 committee vice-Chairs, making UAB the most well-represented institution in the country within the national APSA organization.' +
                         '<br />&nbsp;&nbsp;As our chapter evolves, we continue to increase our involvement in physician scientist development, both locally and nationally.  Anyone interested in joining UAB APSA is welcome to contact us at UAB.APSA@gmail.com or to attend one of our monthly membership meetings.  Thanks, and we encourage you to get involved!' +
                         '<br /><br /><a href="https://uab-apsa.googlecode.com/git/APSAConstitution_2013_01_10.pdf">Download Constitution</a><br /><br /></div>'}).appendTo(main);
@@ -110,7 +111,7 @@ var getImportantDates, importantDates;
     $('<div />', {html: '<a href="#" id="tab4">Past Events</a><div id=pastEvents class= "hide" style="margin-left:10px">' +
         'In addition to monthly meetings the UAB APSA participates in a number of activities. In spring 2013 we helped teach science labs at Parker High School, held a radiation oncology break away session and a women\'s in medicine and research panel. ' +
         'The coming semester we plan on holding a psychiatry break away session and a panel of the physician scientist husbands of our women\'s panel.<br /><br /><div id=eventPictures></div><div><br /></div>'}).appendTo(main);
-    
+
     //Contact Us
     $('<div />', {html: '<a href="#" id="tab7">Contact Us</a><div class= "hide" style="margin-left:10px"><a  href="mailto:UAB.APSA@gmail.com">UAB.APSA@gmail.com</a><br /><br /></div>'}).appendTo(main);
 
@@ -134,15 +135,14 @@ var getImportantDates, importantDates;
 
     //Update with new data
     updateChanges = function () {
-        var webData, editAllSections, updateRoster, sortByLastName, semesterClick, getThisIsMeImages, url, thisIsMeClick, slideClick, slideClickLast,
+        var webData = {}, editAllSections, updateRoster, sortByLastName, semesterClick, getThisIsMeImages, url, thisIsMeClick, slideClick, slideClickLast,
             updateDates, updatePastEvents;
         url = 'https://3fb447c8ea45275c3e71dc49d678c53d6b103efb.googledrive.com/host/0BwdB5oEiQBYWZFk2ZkRNM1d3ZXc/';
-        jQuery.get(url + '?' + (Math.random()).toString().replace('0.',''), function (x) {
+        jQuery.get(url + '?' + (Math.random()).toString().replace('0.', ''), function (x) {
             eval("webData = " + x);
             editAllSections();
         });
         editAllSections = function () {
-            var i;
             console.log(webData);
 
             //update Roster
@@ -158,7 +158,7 @@ var getImportantDates, importantDates;
 
             //Hide all divs
             $('.hide').hide();
-            
+
         };
 
         updateRoster = function (rost) {
@@ -170,19 +170,19 @@ var getImportantDates, importantDates;
             //Populate the div
             div = $('#thisIsMe');
             div.html("");
-            table = $('<table>', {style:"width:100%"}).appendTo(div);
+            table = $('<table>', {style: "width:100%"}).appendTo(div);
             for (i = 0; i < rost.length; i += 1) {
-                if(! (i%2)) {
-                    trow = $('<tr>', {style:"padding:5px;width:100%;"}).appendTo(table);
+                if (!(i % 2)) {
+                    trow = $('<tr>', {style: "padding:5px;width:100%;"}).appendTo(table);
                 }
-                indText = $('<span>', {html: "<b>"+ rost[i].name + "</b> " +rost[i].program}); 
+                indText = $('<span>', {html: "<b>" + rost[i].name + "</b> " + rost[i].program});
                 if (rost[i].hasOwnProperty("office")) {
                     indText.html(indText.html() + ", " + rost[i].office);
                 }
                 if (rost[i].hasOwnProperty('thisIsMe')) {
                     indText.append(getThisIsMeImages(rost[i].name, rost[i].thisIsMe));
                 }
-                $('<td>', {style:"width:50%;padding:5px;", html: indText}).appendTo(trow);
+                $('<td>', {style: "width:50%;padding:5px;", html: indText}).appendTo(trow);
             }
             //Hide everything
             $('.slide').hide();
@@ -193,7 +193,7 @@ var getImportantDates, importantDates;
             var div, i;
 
             //sort dates
-            dates = dates.sort(function (a,b) {
+            dates = dates.sort(function (a, b) {
                 if (new Date(a.date) >= new Date(b.date)) {
                     return 1;
                 }
@@ -207,21 +207,20 @@ var getImportantDates, importantDates;
 
             for (i = 0; i < dates.length; i += 1) {
                 $('<tr>').append(
-                    new Date(dates[i].endTime) - new Date(dates[i].date) > 60*60*1000 ?
-                      $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString() + " - " + (new Date(dates[i].endTime)).toLocaleString() })
-                    :
-                      $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString()})
-                    
+                    new Date(dates[i].endTime) - new Date(dates[i].date) > 60 * 60 * 1000 ?
+                            $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString() + " - " + (new Date(dates[i].endTime)).toLocaleString() })
+                        :
+                                $('<td>', {style: "width:25%;padding:5px;", text: (new Date(dates[i].date)).toLocaleString()})
                 ).append(
                     $('<td>', {style: "padding:5px;", html: "<b>" + dates[i].name + '</b><br />' + dates[i].location + (dates[i].description ? "<br />" + dates[i].description : "") })
                 ).appendTo(div);
-            } 
+            }
         };
 
         updatePastEvents = function (events) {
             //Sort by date
             var semester, i, semStr = "", div;
-            events = events.sort(function (a,b) {
+            events = events.sort(function (a, b) {
                 if (new Date(a.date) >= new Date(b.date)) {
                     return 1;
                 }
@@ -231,11 +230,11 @@ var getImportantDates, importantDates;
             div.text("");
             //Start creating list of events, tab
             for (i = 0; i < events.length; i += 1) {
-                if (events[i].semester != semStr) {
+                if (events[i].semester !== semStr) {
                     //<a href="#" id="tab5">Important Links</a><div class= "hide" style="margin-left:10px">
-                    semester = $('<table>', {class:'semester', style:'margin-left:10px'}).appendTo(
+                    semester = $('<table>', {'class': 'semester', style: 'margin-left:10px'}).appendTo(
                         $('<div>').append(
-                            $('<a>', {href:'#'}).click(semesterClick)
+                            $('<a>', {href: '#'}).click(semesterClick)
                         ).appendTo(div)
                     );
                 }
@@ -249,19 +248,18 @@ var getImportantDates, importantDates;
 
 
             $('.slide').hide();
-            $('.semester').hide();            
+            $('.semester').hide();
         };
 
 
 
         //Sub functions
         getThisIsMeImages = function (name, number) {
-            var i, ret, div, scale;
-            var imageBase = url+"thisIsMeImages"+"/"+encodeURIComponent(name)+"/Slide";
+            var i, ret, div, scale, imageBase = url + "thisIsMeImages" + "/" + encodeURIComponent(name) + "/Slide";
             scale = 2;
             ret = $('<span>', {text: ', '});
-            $('<a>', {href:"#", text:"This is me"}).click(thisIsMeClick).appendTo(ret);
-            div = $('<div>', {class: "slide", style: "border:2px solid black;width:" + 702 / scale + "px;height:" + 540 / scale + 'px;overflow: hidden;margin-left: auto;margin-right: auto', id:'slideContent'}).appendTo(ret);
+            $('<a>', {href: "#", text: "This is me"}).click(thisIsMeClick).appendTo(ret);
+            div = $('<div>', {'class': "slide", style: "border:2px solid black;width:" + 702 / scale + "px;height:" + 540 / scale + 'px;overflow: hidden;margin-left: auto;margin-right: auto', id: 'slideContent'}).appendTo(ret);
             for (i = 1; i <= number; i += 1) {
                 $('<img>', {'class': 'slideIMG', alt: '#', title: 'Click for next slide.', style: "display:block;position:relative;height:" + 540 / scale + 'px;width:' + 702 / scale + "px;", src: imageBase + i + '.jpg'}).click(i === number ? slideClickLast : slideClick).appendTo(div);
             }
@@ -282,13 +280,13 @@ var getImportantDates, importantDates;
         };
 
         slideClick = function (evt) {
-                evt.preventDefault();
-                $(this).hide('slow');
-            };
+            evt.preventDefault();
+            $(this).hide('slow');
+        };
         slideClickLast = function (evt) {
-                evt.preventDefault();
-                $('.slideIMG').show('slow');
-            };
+            evt.preventDefault();
+            $('.slideIMG').show('slow');
+        };
 
         sortByLastName = function (a, b) {
             var aSurname, bSurname;
@@ -304,20 +302,19 @@ var getImportantDates, importantDates;
                 return 1;
             }
             return -1;
-        }
+        };
 
         semesterClick = function (evt) {
-        var text;
-        evt.preventDefault();
-        text = $($(this).parent().children('div')[0]);
-        if (text.is(":visible")) {
-            text.hide('slow');
-        } else {
-            $('.semester').hide('slow');
-            text.show('slow');
-       };
-
-
+            var text;
+            evt.preventDefault();
+            text = $($(this).parent().children('div')[0]);
+            if (text.is(":visible")) {
+                text.hide('slow');
+            } else {
+                $('.semester').hide('slow');
+                text.show('slow');
+            }
+        };
     };
 
 
@@ -446,9 +443,9 @@ var getImportantDates, importantDates;
 
             //Add the images for this person, hiding all but the first one...
             for (j = 0; j < thisIsMeObj[i].images.length; j += 1) {
-                $('<img />', {'class': 'slideIMG', 
-                                alt: '#', title: 'Click for next slide.', 
-                                style: "display:block;position:relative;height:" + 540 / scale + 'px;width:' + 702 / scale + "px;", 
+                $('<img />', {'class': 'slideIMG',
+                                alt: '#', title: 'Click for next slide.',
+                                style: "display:block;position:relative;height:" + 540 / scale + 'px;width:' + 702 / scale + "px;",
                                 src: 'https://uab-apsa.googlecode.com/git/thisIsMeImages/' + thisIsMeObj[i].images[j]}).click(j === thisIsMeObj[i].images.length - 1 ? slideClickLast : slideClick).appendTo('#slideContent' + i);
             }
 
