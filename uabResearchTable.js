f13e6426143b1fe528c781c701cb7f55d220a844
Alex Dussaq <a.m.dussaq@gmail.com>
2014-07-22 12:15:32 -0500

    Hopefully add CSS to table

--- uabResearchTable.js
+++ uabResearchTable.js
@@ -11,7 +11,7 @@ var table = (function () {
     main = {};
     options = {
         visible: {
-            order: [["Audience", "15%"], ["RType", "15%"], ["ProjectSummary", "30%"], ["Department", "15%"], ["Site", "15%"], ["PostDate", "10%"]],
+            order: [["Audience", "15%"], ["RType", "15%"], ["Summary", "30%"], ["Department", "15%"], ["Site", "15%"], ["PostDate", "10%"]],
             Audience: ["Undergraduate Student - Summer", "Undergraduate Student - Academic Year", "Medical Student - Summer", "Medical Student - Scholarly Activity", "Graduate or MD/PhD Student", "Resident"],
             RType: ["Basic science", "Translational science", "Clinical Research", "Behavioral Research", "Chart Review", "Community-based", "Education", "Public Health/Epidemiology", "Health Outcomes", "Other"],
             Summary: wordSearch,
@@ -59,7 +59,10 @@ var table = (function () {
         data = JSON.parse(x);
         dict = data.dict;
         data = data.data;
-        table = $("<table>", {style: "width:100%"}).appendTo(div);
+        table = $("<table>", {"class": "CSSTableGenerator", style: "width:100%"}).appendTo(div);
+
+        //set css
+        $("<style type='text/css'>.CSSTableGenerator{margin:0;padding:0;width:100%;box-shadow:10px 10px 5px #888;border:1px solid #1e6b52;-moz-border-radius-bottomleft:0;-webkit-border-radius:0;border-radius:0;-moz-border-radius-bottomright:0;-moz-border-radius-topright:0;-moz-border-radius-topleft:0}.CSSTableGenerator table{border-collapse:collapse;border-spacing:0;width:100%;height:100%;margin:0;padding:0}.CSSTableGenerator tr:last-child td:last-child{-moz-border-radius-bottomright:0;-webkit-border-bottom-right-radius:0;border-bottom-right-radius:0}.CSSTableGenerator table tr:first-child td:first-child{-moz-border-radius-topleft:0;-webkit-border-top-left-radius:0;border-top-left-radius:0}.CSSTableGenerator table tr:first-child td:last-child{-moz-border-radius-topright:0;-webkit-border-top-right-radius:0;border-top-right-radius:0}.CSSTableGenerator tr:last-child td:first-child{-moz-border-radius-bottomleft:0;-webkit-border-bottom-left-radius:0;border-bottom-left-radius:0}.CSSTableGenerator tr:nth-child(odd){background-color:#d4ffaa}.CSSTableGenerator tr:nth-child(even){background-color:#fff}.CSSTableGenerator td{vertical-align:middle;border:1px solid #1e6b52;border-width:0 1px 1px 0;text-align:left;padding:5px;font-size:10px;font-family:Arial;font-weight:400;color:#000}.CSSTableGenerator tr:last-child td{border-width:0 1px 0 0}.CSSTableGenerator tr td:last-child{border-width:0 0 1px}.CSSTableGenerator tr:last-child td:last-child{border-width:0}.CSSTableGenerator tr:first-child td{background:#1e6b52 -webkit-gradient(linear,left top,left bottom,color-stop(0.05,#1e6b52),color-stop(1,#a3d55d));background:#1e6b52 -moz-linear-gradient(center top,#1e6b52 5%,#a3d55d 100%);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#1e6b52', endColorstr='#a3d55d');background:#1e6b52 -o-linear-gradient(top,#1e6b52,a3d55d);border:0 solid #1e6b52;text-align:center;border-width:0 0 1px 1px;font-size:14px;font-family:Arial;font-weight:700;color:#fff}.CSSTableGenerator tr:first-child:hover td{background:#1e6b52 -webkit-gradient(linear,left top,left bottom,color-stop(0.05,#1e6b52),color-stop(1,#a3d55d));background:#1e6b52 -moz-linear-gradient(center top,#1e6b52 5%,#a3d55d 100%);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#1e6b52', endColorstr='#a3d55d');background:#1e6b52 -o-linear-gradient(top,#1e6b52,a3d55d)}.CSSTableGenerator tr:first-child td:first-child{border-width:0 0 1px}.CSSTableGenerator tr:first-child td:last-child{border-width:0 0 1px 1px}</style>").appendTo(div);
 
         //make table header
         row = $("<tr>", {style: "width:100%;padding:5px"}).appendTo(table);
@@ -86,10 +89,10 @@ var table = (function () {
 
     getList = function (arr) {
         var ret = "";
-        if (typeof arr === 'string') {
+        if (typeof arr === 'string' || !arr) {
             ret = arr;
         } else {
-            arr.map(function (x) {ret = ret + ", " + x; });
+            arr.map(function (x) {ret = ret ? ret + ", " + x : x; });
         }
         return ret;
     };
