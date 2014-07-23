68979efd77d14b33d666b438b97c0dc5fe7b3a4b
Alex Dussaq <a.m.dussaq@gmail.com>
2014-07-23 12:11:05 -0500

    Let us hope I can remember scoping and how to string together functional programs.

--- uabResearchTable.js
+++ uabResearchTable.js
@@ -1,7 +1,7 @@
 /*global console, $, jQuery */
 var table = (function () {
     'use strict';
-    console.log("v0.1.9");
+    console.log("v0.1.10");
     //variables
     var filterMaker, tableRows, updateData, pageText, rightClick, leftClick, cPage, maxPage, getList, dict, options, main, makeTable, makeTableBody, $, div, data, dataArr, startBuilding, wordSearch, order, perPage;
 
