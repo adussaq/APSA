ae51121f741cab238d2aa27dcc5b646ca4de3f2b
Alex Dussaq <a.m.dussaq@gmail.com>
2014-08-18 14:14:22 -0500

    change appendTo

--- ASPA.js
+++ ASPA.js
@@ -1,6 +1,6 @@
 /*global console, $, jQuery */
 var updateChanges;
-console.log("v2.2.0");
+console.log("v2.2.1");
 //Tracking
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
