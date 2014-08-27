1afa58f25e96e86ebb02d0aa86329389bb65ae23
Alex Dussaq <a.m.dussaq@gmail.com>
2014-08-27 15:52:57 -0500

    update v#

--- ASPA.js
+++ ASPA.js
@@ -1,6 +1,6 @@
 /*global console, $, jQuery */
 var updateChanges;
-console.log("v2.2.6");
+console.log("v2.2.7");
 //Tracking
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
