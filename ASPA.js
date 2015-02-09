75b1994b4789a04bb741596edd45223365ea0421
Alex <a.m.dussaq@gmail.com>
2015-02-09 14:12:09 -0600

    update apsa stuff to remove unused sections

--- ASPA.js
+++ ASPA.js
@@ -1,6 +1,6 @@
 /*global console, $, jQuery */
 var updateChanges;
-console.log("v2.2.15");
+console.log("v2.2.16");
 //Tracking
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
